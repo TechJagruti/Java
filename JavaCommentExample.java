ce34e67ffea6ec988aad881e692ee0fcd60ef8ee
TechJagruti <techjagruti@gmail.com>
2026-02-14 13:07:53 +0530

    Create JavaCommentExample.java

---
+++ JavaCommentExample.java
@@ -0,0 +1,26 @@
+/**
+ * This is a documentation comment.
+ * It describes the purpose of the program.
+ * Program: Demonstrates different types of comments in Java.
+ */
+public class CommentExample {
+
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("Hello, World!");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         * Useful for longer explanations.
+         */
+        int a = 10;
+        int b = 20;
+
+        // Adding two numbers
+        int sum = a + b;
+
+        System.out.println("Sum of a and b is: " + sum);
+    }
+}
