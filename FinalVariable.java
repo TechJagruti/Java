6a9760170788a4640780a873c64ef6d329843c8f
TechJagruti <techjagruti@gmail.com>
2026-02-16 15:11:55 +0530

    Create FinalVariable.java

---
+++ FinalVariable.java
@@ -0,0 +1,9 @@
+class Demo {
+    public static void main(String arrr[]) {
+        final float pi = 3.14 f;
+        float area, r = 10;
+        area = pi * r * r;
+        pi = 3.6f;
+        System.out.println("Area=" + area);
+    }
+}
