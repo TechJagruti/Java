c2fcaac415234ba13efa864898bd1803df8604f9
TechJagruti <techjagruti@gmail.com>
2026-02-16 12:40:31 +0530

    Create StackClass.java

---
+++ StackClass.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Demo{
+    public static void main (String arr []){
+    Stack s=new Stack ();
+    s.push("Anil");
+    s.push("Sunil* ) ;
+    s.push (Ajay);
+    s.push("Achal");
+    s.push("Aniket");
+    System.out.println("Elements of stack: "+s);
+    System.out.println("Result of pop method: s.pop());
+    System.out.println("Elements of stack after pop: +s);
+    System.out.println("Result of peek method: +s.peek());
+    System.out.println("Elements of stack after peek: "+s);
+    System.out.println("Is empty: " + s.empty());
+    System.out.println("offset of Sunil is: +s.search("Sunil"));
+    }
+}
