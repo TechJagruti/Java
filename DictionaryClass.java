c1d5d06b51dd198c763cbc44b501d87c24d688e2
TechJagruti <techjagruti@gmail.com>
2026-02-16 12:39:03 +0530

    Create DictionaryClass.java

---
+++ DictionaryClass.java
@@ -0,0 +1,17 @@
+import java.util.Dictionary;
+import java.util.Hashtable;
+public class Demo{
+    public static void main ( string args[]){
+    Hashtable ob1 = new Hashtable () ;
+    Double to = new Double(258);
+    Integer i = new Integer (45) ;
+    obl.put("a", "akbar");
+    obl.put("b", d);
+    obl.put("c", "birbal");
+    obl.put("d", i);
+    System.out.println ("a: " + ob1.get ("a" ) ) ;
+    System.out.println("b:+obi.get("b"));
+    System.out.println("c: + ob1.get (*c")); "
+    System.out.println ("d: • " + ob1.get ("d* ) ) ;
+    }
+}
