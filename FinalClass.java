8aa70081c1697567af101a8ed7108b729dee9fd9
TechJagruti <techjagruti@gmail.com>
2026-02-16 15:17:38 +0530

    Create FinalClass.java

---
+++ FinalClass.java
@@ -0,0 +1,27 @@
+final class A {
+    int a;
+    void get(int x) {
+        a = x;
+    }
+    void show() {
+        System.out.println("a=" + a);
+    }
+}
+class B extends A {
+    int b;
+    void getb(int x, int y) {
+        a = x;
+        b = y;
+    }
+    void show() {
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+}
+class Demo {
+    public static void main(String arrr[]) {
+        B ob = new B();
+        ob.getb(23, 34);
+        ob.show();
+    }
+}
