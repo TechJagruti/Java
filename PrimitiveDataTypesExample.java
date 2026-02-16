285459781db95522d3612c9072cc85092e3e61a1
TechJagruti <techjagruti@gmail.com>
2026-02-16 10:36:32 +0530

    Create PrimitiveDataTypesExample.java

---
+++ PrimitiveDataTypesExample.java
@@ -0,0 +1,39 @@
+public class PrimitiveDataTypesExample {
+
+    public static void main(String[] args) {
+
+        // 1. boolean data type
+        boolean isJavaEasy = true;
+
+        // 2. byte data type
+        byte byteValue = 100;
+
+        // 3. char data type
+        char grade = 'A';
+
+        // 4. short data type
+        short shortValue = 20000;
+
+        // 5. int data type
+        int intValue = 150000;
+
+        // 6. long data type
+        long longValue = 1234567890L;   // L is required
+
+        // 7. float data type
+        float floatValue = 10.5f;   // f is required
+
+        // 8. double data type
+        double doubleValue = 99.99;
+
+        // Printing all values
+        System.out.println("Boolean value: " + isJavaEasy);
+        System.out.println("Byte value: " + byteValue);
+        System.out.println("Char value: " + grade);
+        System.out.println("Short value: " + shortValue);
+        System.out.println("Int value: " + intValue);
+        System.out.println("Long value: " + longValue);
+        System.out.println("Float value: " + floatValue);
+        System.out.println("Double value: " + doubleValue);
+    }
+}
