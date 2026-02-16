5e5f185316d2ea0a86a1e586f73439225ea6bb56
TechJagruti <techjagruti@gmail.com>
2026-02-16 13:05:39 +0530

    Create StringObjectUsingTheStringClass.java

---
+++ StringObjectUsingTheStringClass.java
@@ -0,0 +1,9 @@
+class Str {
+public static void main(String arr[]){
+    String s1="Hello";
+    String s2="World";
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s1+s2);
+    }
+}
